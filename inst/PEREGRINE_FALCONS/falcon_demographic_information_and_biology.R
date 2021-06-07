403e757ca3971e5291296ca8db5f6dfdfbddbd09
Nathan Brouwer <brouwern@gmail.com>
2021-06-07 13:58:15 -0400

    update for Brent

---
+++ inst/PEREGRINE_FALCONS/falcon_demographic_information_and_biology.R
@@ -0,0 +1,104 @@
+http://midwestperegrine.umn.edu/?vw=statistics
+
+#Comparison of nest sites
+#In 2003, of 163 Midwest territorial pairs, 40% were on buildings, 17% on smokestacks, 10% on bridges, 
+#and 32% on cliffs. 
+#From 1987 through 2003, smokestack pairs fledged 2.8 young per nest, 
+#building pairs 2.4 young per nest, 
+#cliff pairs 2.0, bridge pairs 1.8.
+
+
+Effect of fledge site
+
+Adults tend to use nest sites similar to the site they fledged from (building, smokestack, bridge, or cliff), but this preference is far from absolute. Males are more likely than females to use nest sites like their fledge sites. Of 164 females, 101 (59%) used a nest site like the site they fledged from. Of 137 males, 100 (73%) used a nest site like their fledge site.
+
+
+### Age of first breeding
+# 
+# Probably all peregrines are physiologically ready to 
+# breed by age two, but actual first breeding varies, 
+# depending on availability of territorial vacancies. 
+# Females tend to breed at younger age than males. 
+# Of 144 females in the Midwest, 15 % first nested at age 
+# one, 58 % at age two, 17 % at age three, 7 % at age four,
+# 2 % at age five, and 1% at age six. By contrast, of 
+# 136 males, 8 % first nested at age one, 46 % at age 
+# two, 20 % at age three, 17 % at age four, 5 % at age 
+# five, and 4% at age six.
+
+
+
+
+Lifetime reproduction
+
+Varies hugely; maximum known for one female, 43 young fledged (10 females in the Midwest have fledged 25 or more young); maximum for one male, 37 young (10 males have fledged 26 or more young). Average for Midwest population obviously much lower, but not yet calculated.
+
+
+
+
+Causes of mortality
+
+In 455 recorded fatalities among Midwestern peregrines, the cause of death was determined for 243: 32 % were caused by collisions with buildings, 21 % by vehicle collisions, 14 % by miscellaneous accidents, 10 % by disease and starvation, 8 % by adult peregrines, 6 % by predators, 4 % by shooting, 4 % by storms, lightning, and other minor causes.
+
+
+
+###Survival of breeders
+
+#In the Midwest, 1987 to 2001, annual survival of 
+#375 male breeders averaged 83%; 
+#for 446 female breeders, 86%. 
+#Judging by rapidity of replacement of lost breeders, 
+#a substantial population of non-breeding floating 
+#adults exists, whose annual survival is unknown.
+
+
+#Broods per year
+#One. May re-nest after loss of eggs; the earlier the loss, the more likely the re-nesting.
+
+
+#*#*#*#* Clutch size 
+# Most frequently 4 in Midwest. Of 525 nests, 64% had 4 eggs, 
+# 21% had three, 7% had 5, 6% had 2, and 2% had 1 egg.
+
+
+#### Brood size at fledging
+# 
+# 2.8 young per successful pair (N = 858 pairs), 
+# 2.3 per nesting pair (N = 1035), 
+# 1.9 per territorial pair (N = 1253).
+
+
+
+
+
+###Incubation period
+#33 to 35 days.
+
+###Nestling period
+#40 to 45 days.
+
+
+#Longevity, length of reproductive life
+#Up to 20 years in the wild; oldest female in Midwest, still alive in 2004, is 18 years old and still holding a territory in St. Paul. She has nested 16 times, has had 6 mates, laid 60 eggs, and fledged 43 young. Her last successful nest was in 2001 at age 15. In 2002, she laid 3 eggs that did not hatch. In 2003, she laid one abnormally shaped egg. The oldest male, also 18 and still alive in Chicago, sired 5 young in 2003. Most peregrines die long before they reach old age; average age at death of 61 Midwest females was 5.6 years, of 64 males, 4.5 years.
+
+
+
+
+#Dispersal from natal site
+#In Midwest, 67 females moved an average of 345 km from their natal site to their breeding site, 73 males moved an average of 174 km. In other populations studied, females typically disperse about twice as far as males.
+
+
+#Fidelity to nest site
+#In Midwest, 1987 through 1995, of 241 territory-years, only 10 adults moved to a new territory. Females are more likely to move than males.
+
+
+First year survival
+
+Assumed to be 40% to 50%, but no reliable study available.
+
+
+
+Migration
+
+In Midwest, many urban peregrines are resident year around. Birds on cliffs and some urban birds migrate, but routes, destinations, and timing poorly known. Paired birds do not migrate together.
+
